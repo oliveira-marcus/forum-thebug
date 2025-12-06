87b011ac45d9c3d8bcb732aa219efc5defcf2646
Caio Bruno <deganaqualy@gmail.com>
2025-12-06 11:17:18 -0300

    feat: Tornando Header responsivo

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,8 +1,8 @@
-import { Search, Menu, LogOut, LucideSearch } from "lucide-react";
+import { Search, Menu, LogOut } from "lucide-react";
 import { useAuth } from "../../contexts/AuthContext";
 import { useNavigate } from "react-router";
 import usePostSearch from "../../hooks/usePostSearch";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 interface HeaderProps {
   heading: string;
@@ -11,6 +11,8 @@ interface HeaderProps {
 
 export default function Header({ heading, subheading }: HeaderProps) {
   const [query, setQuery] = useState<string>("");
+  const [isMobileSearchActive, setIsMobileSearchActive] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
   const [isFocused, setIsFocused] = useState<boolean>(false);
 
   const { signOut } = useAuth();
@@ -31,34 +33,66 @@ export default function Header({ heading, subheading }: HeaderProps) {
   return (
     <header className="bg-linear-to-r from-blue-600 to-blue-800 border-b border-blue-700 sticky top-0 z-50">
       <div className="max-w-10xl mx-auto px-4 py-3">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center gap-4">
-            <button className="p-2 hover:bg-blue-700 rounded-lg transition md:hidden">
-              <Menu className="w-6 h-6" />
+        <div className="flex items-center">
+          <div className={`${
+                isMobileSearchActive && "hidden"
+              } flex items-center gap-4`}>
+            <button
+              onClick={() => {}}
+              aria-label="Toggle sidebar"
+              className={`cursor-pointer p-3 rounded-full hover:bg-blue-500 transition`}
+            >
+              <Menu size={28} />
             </button>
-            <img
-              src="/src/assets/logo_round.png"
-              alt="Logo The Bug"
-              className="w-12 h-12 rounded-full object-cover border-2 border-white"
-            />
-            <div>
-              <h1 className="text-2xl font-bold">{heading}</h1>
-              <p className="text-blue-200 text-sm">{subheading}</p>
+
+            <div
+              className={`${
+                isMobileSearchActive && "hidden"
+              } flex items-center gap-4`}
+            >
+              <img
+                src="/src/assets/logo_round.png"
+                alt="Logo The Bug"
+                className="w-12 h-12 rounded-full object-cover border-2 border-white"
+              />
+              <div className="hidden sm:block">
+                <h1 className="text-2xl font-bold">{heading}</h1>
+                <p className="text-blue-200 text-sm">{subheading}</p>
+              </div>
             </div>
           </div>
 
-          <div className="flex flex-1 max-w-[30%] items-center gap-3">
-            <div className="flex flex-1 ml-4 sm:flex z-0 relative py-2 rounded-2xl gap-4 bg-[#122B83]">
-              <LucideSearch size={24} color="white" className="ml-4" />
+          <div className="flex flex-1 items-center pl-8">
+            <div
+              className={`${isMobileSearchActive && "hidden"} sm:hidden h-6 ml-auto`}
+            >
+              <button
+                onClick={() => {
+                  setIsMobileSearchActive(true);
+                  setTimeout(() => inputRef.current?.focus(), 150);
+                }}
+              >
+                <Search size={24} color="white" />
+              </button>
+            </div>
+
+            <div
+              className={`${!isMobileSearchActive && "hidden"} ${
+                isMobileSearchActive && "flex flex-1"
+              } sm:flex z-0 mx-auto min-w-56 w-[30%] relative py-2 rounded-2xl gap-4 bg-[#122B83]`}
+            >
+              <Search size={24} color="white" className="ml-4" />
 
               <input
-                className="text-white placeholder-blue-300 focus:outline-none bg-transparent w-full"
+                className="text-white placeholder-blue-300 focus:outline-none bg-transparent min-w-44"
+                ref={inputRef}
                 placeholder="Buscar discussões..."
                 type="text"
                 value={query}
                 onChange={handleQueryChange}
                 onFocus={() => setIsFocused(true)}
                 onBlur={() => {
+                  setIsMobileSearchActive(false);
                   setIsFocused(false);
                 }}
               />
@@ -79,9 +113,9 @@ export default function Header({ heading, subheading }: HeaderProps) {
             </div>
           </div>
 
-          <div>
+          <div className="ml-auto">
             <button
-              className="p-2 hover:bg-blue-700 rounded-lg transition cursor-pointer"
+              className="p-2 hover:bg-blue-700 rounded-lg transition cursor-pointer "
               onClick={handleSignOut}
             >
               <LogOut className="w-6 h-6" />
